249e6dd1c442a23b7f9b0af55e10ad300db594a7
Evgeniy <svorovali@mail.ru>
2023-08-01 14:25:39 +0300

    create  file

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,14 +2,17 @@ import java.util.function.*;
 
 public class Calculator {
     public Calculator() {
-
     }
 
     static Supplier<Calculator> instance = Calculator::new;
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) -> x / y;
+    BinaryOperator<Integer> divide = (x, y) -> {
+        if (y == 0 || x == 0){
+           return 0;
+        } else return x/y;
+    };
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPosivive = x -> x > 0;
--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public class Main {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);  // Получится 0
+        int c = calc.divide.apply(a, b);
+        System.out.println(c);
         //int c  = calc.divide.apply(a,b);  // На ноль делить нельзя
         //  Можно добавить проверку
-        Optional<Integer> result = b != 0 ? Optional.of(calc.divide.apply(a, b)) : Optional.empty();
-        result.ifPresentOrElse(calc.println, () -> System.out.println("0"));
+         // Optional<Integer> result = b != 0 ? Optional.of(calc.divide.apply(a, b)) : Optional.empty();
+       // result.ifPresentOrElse(calc.println, () -> System.out.println("0"));
         // calc.println.accept(c);
     }
 }
