9167b694b1405c015f975e206359f7995539b24d
Evgeniy <svorovali@mail.ru>
2023-08-01 10:24:44 +0300

    File init

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);  // Получится 0
+        //int c  = calc.divide.apply(a,b);  // На ноль делить нельзя
+        //  Можно добавить проверку
+        Optional<Integer> result = b != 0 ? Optional.of(calc.divide.apply(a, b)) : Optional.empty();
+        result.ifPresentOrElse(calc.println, () -> System.out.println("0"));
+        // calc.println.accept(c);
+    }
+}
